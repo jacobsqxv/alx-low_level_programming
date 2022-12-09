eebf12d0ecf924388490e9d006008682428b67f0
jacobsqxv <agyeijacobs@outlook.com>
2022-12-09 06:12:17 -0800

    Print alphabets minus e and q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,11 +9,10 @@ int main(void)
 
 	for (c = 'a'; c <= 'z'; c++)
 	{
-		if (c == 'e' || c == 'q')
+		if (c != 'e' && c != 'q')
 		{
-			continue;
+			putchar(c);
 		}
-		putchar(c);
 	}
 	putchar('\n');
 	return (0);
