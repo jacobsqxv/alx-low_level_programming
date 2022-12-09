697b385ebee9763a2e4e4ede87f2acd2324ca64d
jacobsqxv <agyeijacobs@outlook.com>
2022-12-09 06:04:46 -0800

    Print alphabets minus e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c == 'e' || c == 'q')
+		{
+			continue;
+		}
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
