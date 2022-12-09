7edd7b816b8168fddff55cc7bf03a668ff601602
jacobsqxv <agyeijacobs@outlook.com>
2022-12-09 05:44:51 -0800

    Print all alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: using main function
+ * this program prints alphabets in lower case
+ * Return: 0
+ */
+int main(void)
+{
+        char c;
+
+        for (c = 'a'; c <= 'z'; c++)
+        {
+                putchar(c);
+        }
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+        putchar('\n');
+        return (0);
+}
