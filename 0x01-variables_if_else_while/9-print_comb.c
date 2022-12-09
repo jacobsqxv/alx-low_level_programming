6336de4df25dd1ef5ba6138b89a3bc43b3487d06
jacobsqxv <agyeijacobs@outlook.com>
2022-12-09 08:51:29 -0800

    Print combination

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,9 +14,8 @@ int main(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		i = i % 10 + '0';
-		putchar(i);
-		if (i != '9')
+		putchar(i % 10 + '0');
+		if (i != 9)
 		{
 			putchar(',');
 			putchar(' ');
