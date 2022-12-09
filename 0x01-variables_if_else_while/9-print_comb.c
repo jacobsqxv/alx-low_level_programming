d26a40df87b12249669c95add9fe8fe3af534f84
jacobsqxv <agyeijacobs@outlook.com>
2022-12-09 08:45:58 -0800

    Print combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: using the main function
+ * this program prints "0, 1, 2, 3, 4, 5, 6, 7, 8, 9"
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		i = i % 10 + '0';
+		putchar(i);
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n';
+	return (0);
+}
