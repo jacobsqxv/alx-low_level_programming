fc162fa84257e94af5a7cfb52309f1f5facde01e
jacobsqxv <agyeijacobs@outlook.com>
2022-12-09 08:08:43 -0800

    Print numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -16,7 +16,7 @@ int main(void)
 
         for (c = 0; c <= 9; c++)
         {
-                putchar(c);
+                putchar(c%10+'O');
         }
         putchar('\n');
         return (0);
