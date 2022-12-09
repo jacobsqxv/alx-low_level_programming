09742955bc54e364f7a80666faa73227a80bec2c
jacobsqxv <agyeijacobs@outlook.com>
2022-12-09 07:46:35 -0800

    Print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: using main function
+ * this program prints alphabets in lower case
+ * Return: 0
+ */
+int main(void)
+{
+        int c;
+
+        for (c = 0; c <= 9; c++)
+        {
+                putchar(c);
+        }
+        putchar('\n');
+        return (0);
+}
