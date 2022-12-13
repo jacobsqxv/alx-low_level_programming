53b9268ea5112460469ef5b1b1b8a1b273e16806
jacobsqxv <agyeijacobs@outlook.com>
2022-12-13 05:02:02 -0800

    Print alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: using main function
+ * print alphabets
+ * Return: 0
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
