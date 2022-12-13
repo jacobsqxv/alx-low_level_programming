72209b942989537ad04d649a0d09e9c279a5bafa
jacobsqxv <agyeijacobs@outlook.com>
2022-12-13 05:28:42 -0800

    Print alphabets 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,14 +9,16 @@
 void print_alphabet_x10(void)
 {
 	int i = 0;
+
 	while (i < 10)
 	{
 		char c;
+		
 		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
 		}
+	_putchar('\n');
 	i++;
 	}
-	_putchar('\n');
 }
