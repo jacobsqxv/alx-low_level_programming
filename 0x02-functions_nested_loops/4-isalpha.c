143c1ac7c53b85f40e74f31b4e874014f7d0870f
jacobsqxv <agyeijacobs@outlook.com>
2022-12-13 06:30:58 -0800

    Print 0 or 1 based on condition

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _isalpha: Entry point
+ * @c: input character
+ * Description: Print 1 if c is letter and 0 if otherwise
+ * Return: 0
+ */
+
+int _isalpha(int c)
+{
+	if (isalpha(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
