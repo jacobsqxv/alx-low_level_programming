0860ef8128a1e20452926b68cdebe23622b6c487
jacobsqxv <agyeijacobs@outlook.com>
2022-12-13 07:00:09 -0800

    Print absolute of number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - entry point
+ * Description: print absolute value of integer
+ * Return: 0
+ */
+
+int _abs(int)
+{
+	int i;
+
+	i = abs(i);
+	_putchar(i);
+	return (0);
+}
