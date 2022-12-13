a4c63e47c67df5ad4aed13c385fba8e9e99825cc
jacobsqxv <agyeijacobs@outlook.com>
2022-12-13 05:25:54 -0800

    Print alphabets 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ * Description: print alphabets 10 times
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	while (i < 10)
+	{
+		char c;
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+	i++;
+	}
+	_putchar('\n');
+}
