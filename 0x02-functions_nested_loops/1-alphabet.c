7b2e47b068468c254395b188184dfc544e23d596
jacobsqxv <agyeijacobs@outlook.com>
2022-12-13 05:16:44 -0800

    Print alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,19 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * print_alphabet - Entry point
  *
- * Description: using main function
- * print alphabets
+ * Description: print alphabets
  * Return: 0
  */
 
 void print_alphabet(void)
 {
 	char c;
-	
+
 	for (c = 'a'; c <= 'z'; c++)
 	{
 		putchar(c);
 	}
 	putchar('\n');
-	return;
 }
