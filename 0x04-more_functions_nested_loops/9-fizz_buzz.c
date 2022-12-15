190610d0b10397c0e6639fa5e810e363c17df66c
jacobsqxv <agyeijacobs@outlook.com>
2022-12-15 08:31:05 -0800

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,23 +10,28 @@ int main(void)
 {
 	int i = 1;
 
-	while (i <= 100)
+	while (i < 101)
 	{
 		if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("%s","Fizz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("%s","Buzz");
 		}
 		else if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("%s", "FizzBuzz");
 		}
 		else
 		{
 			printf("%d ",i);
+		}
+
+		if (i != 100)
+		{
+			printf(" ");
 		}
 		
 		i++;
