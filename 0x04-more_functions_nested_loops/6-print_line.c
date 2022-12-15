b067996aea7ec1cedfa7cb2de627c0d4fbdf347e
jacobsqxv <agyeijacobs@outlook.com>
2022-12-15 07:30:04 -0800

    Print lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - function
+ * @n: input integer
+ * Description: print lines
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	while (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
