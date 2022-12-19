80b274e47c7ee8edf15a2a9a462d609f9aca914f
jacobsqxv <agyeijacobs@outlook.com>
2022-12-19 10:37:27 -0800

    Copy string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcpy - function
+ * dest: destination character
+ * src: source character
+ * Description: copy string to another destination
+ * Return: void
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i]; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = src[i];
+	return (dest);
+}
