637eec281bf07ecdd31a85398fb34973d9b7c40b
jacobsqxv <agyeijacobs@outlook.com>
2022-12-19 07:12:52 -0800

    Update pointer to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function
+ * @n: input array
+ * Description: update pointer to print 98
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
