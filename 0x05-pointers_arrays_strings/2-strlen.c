9e47ef2d6db94edcfc9ac5119633162dbd000f55
jacobsqxv <agyeijacobs@outlook.com>
2022-12-19 07:26:39 -0800

    Return length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strlen - function
+ * @s: input string
+ * Description: return length of string
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	return(strlen(s));
+}
