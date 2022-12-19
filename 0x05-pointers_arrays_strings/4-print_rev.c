c68ae050141232ed46c25f7a8dbd9318d0fba5d6
jacobsqxv <agyeijacobs@outlook.com>
2022-12-19 07:43:23 -0800

    Print string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_rev - function
+ * @s: input string
+ * Description: print string in reverse
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	puts(strrev(s));
+}
