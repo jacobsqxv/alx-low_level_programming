b6324744ecc1216d23f7140c8259004c34cd52d3
jacobsqxv <agyeijacobs@outlook.com>
2022-12-21 13:45:10 -0800

    Concatenate 2 strings with n bytes

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strncat - function
+ * @n: size of bytes to concatenate
+ * @dest: first string
+ * @src: second string
+ * Description: concatenate 2 strings with n bytes
+ * Return: 0
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, c = 0, s = 0;
+
+	while 
