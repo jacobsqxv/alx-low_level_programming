3712474363a1a2c94f9cabee1abe8daad1c36d79
jacobsqxv <agyeijacobs@outlook.com>
2023-01-12 07:59:38 -0800

    Multiply two positive integers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -9,14 +9,14 @@
 
 int main(int argc, char *argv[])
 {
-  unsigned long mul;
-  int i, j;
-  
+	unsigned long mul;
+	int i, j;
+
 	if (argc != 3)
 	{
-    printf("Error\n");
-	  exit(98); 
-  }
+		printf("Error\n");
+		exit(98);
+	}
 
 	for (i = 1; i < argc; i++)
 	{
@@ -24,12 +24,12 @@ int main(int argc, char *argv[])
 		{
 			if (argv[i][j] > 57 || argv[i][j] < 48)
 			{
-        printf("Error\n");
-        exit(98);
-      }
+				printf("Error\n");
+				exit(98);
+			}
 		}
 	}
 	mul = atol(argv[1]) *  atol(argv[2]);
 	printf("%lu\n", mul);
-  return (0);
+	return (0);
 }
