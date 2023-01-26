ef568ff76b50c3aae7822a6f1ff345f69f3dc57e
Jacobs Agyei <57623419+jacobsqxv@users.noreply.github.com>
2023-01-26 22:15:39 +0000

    Print string before main function

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,16 +1,14 @@
 #include<stdio.h>
 
 /**
- * myStartupFun - Apply the constructor attribute to myStartupFun()
+ * myStartupFun - apply the constructor attribute to myStartupFun()
  * to execute before main()
  */
-
 void myStartupFun(void) __attribute__ ((constructor));
 
 /**
  * myStartupFun - implementation of myStartupFun
  */
-
 void myStartupFun(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
