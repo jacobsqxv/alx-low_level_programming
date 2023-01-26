db75cd7a91adb0b6bc4d147e63900456435ea1a9
Jacobs Agyei <57623419+jacobsqxv@users.noreply.github.com>
2023-01-26 22:10:40 +0000

    Assembly code to print string

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,21 @@
+		extern	printf		; function to be called
+
+	        section .data		; initialize variables
+msg:		db "Hello, Holberton", 0 ; string needs 0
+fmt:	    	db "%s", 10, 0          ; printf format, "\n",'0'
+
+	        section .text		; Code section.
+
+	        global main		; standard gcc entry point
+main:					; the program label for the entry point
+	        push    rbp		; set up stack frame, must be alligned
+
+		mov	rdi,fmt
+		mov	rsi,msg
+		mov	rax,0		
+	        call    printf		; Call C function
+
+		pop	rbp		; restore stack
+
+		mov	rax,0		; normal, no error, return value
+		ret			; return
